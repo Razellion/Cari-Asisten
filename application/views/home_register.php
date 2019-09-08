1caa39499a6969685297d72396942ab587c43771
zahrinaadriani <zahrinaadriani@gmail.com>
2019-09-09 00:38:02 +0700

    push zahrina:)

---
+++ application/views/home_register.php
@@ -0,0 +1,18 @@
+	<div>
+		<div class="container-create"style="width: 500px;height: 400px;background-color:white;color:black;margin-left: 650px; margin-top: 50px; border-radius: 15px; border: 2px solid black;">
+			<p style=" padding-top :10px; text-align: center; font-family: cursive; font-size: 25px;"> Create New Account</p>
+			<div>
+				<button  style=" margin-top :40px;border:1px solid black; margin-left :35px; margin-bottom: 40px; background-color: #00FA9A;  height: 50px;width: 320px; float: left;"><p style=" font-weight:bold; font-family: cursive; padding-top:5px; text-align: center;"> Register as Student </p></button>
+				<img src="<?php echo base_url('assets/image/student.png');?>" style=" margin-top: 30px; margin-right :27px; float:right; width: 65px;height: 60px;" />
+			</div>
+			<div>
+				<button  style=" margin-top :40px;border:1px solid black; margin-left :35px; margin-bottom: 40px; background-color: #4682B4;  height: 50px;width: 320px; float: left;"><p style=" font-weight:bold; font-family: cursive; padding-top:5px; padding-left :10px; text-align: center;"> Register as Lecturer </p></button>
+				<img src="<?php echo base_url('assets/image/lecture.jpg');?>" style=" margin-top: 30px; margin-right :20px;  float:right; width: 80px;height: 80px;" />
+			</div>
+			
+			
+
+		</div>
+	</div>
+</body>
+</html>
---
+++ application/views/register_student.php
@@ -0,0 +1,38 @@
+	<div>
+		<form>
+			<div class="container-create"style="width: 500px;height: 430px;background-color:white;color:black;margin-left: 650px; margin-top: 50px; border-radius: 15px; border: 2px solid black;">
+			<h1 style=" margin-bottom:10px; font-size: 30px; padding-left: 10px; padding-top: 10px; text-align: left; font-family: cursive;">Register as Student</h1>
+			<div style="margin-top: 35px;height: 250px;width: 380px;margin-left: 15px;">
+				<div class="row" style="margin-bottom: 20px;">
+					<div class="col-sm-4"> Full Name: </div>
+					<div class="col-sm-8"><input style=" width :300px; border: 2px solid black;" type="text" id="fname" name="Name"></div>
+				</div>
+				<div class="row" style="margin-bottom: 20px;">
+					<div class="col-sm-4"> NIM: </div>
+					<div class="col-sm-8"><input style="width :300px; border: 2px solid black;" type="text" id="fnim" name="NIM"></div>
+				</div>
+				<div class="row" style="margin-bottom: 20px;">
+					<div class="col-sm-4"> Email: </div>
+					<div class="col-sm-8"><input style="width :300px; border: 2px solid black;" type="text" id="femail" name="EMAIL"></div>
+				</div>
+				<div class="row" style="margin-bottom: 20px;">
+					<div class="col-sm-4">Password: </div>
+					<div class="col-sm-8"><input style="width :300px; border: 2px solid black;"  type="text" id="fpassword" name="PASS"></div>
+				</div>
+				<div class="row" style="margin-bottom: 20px;">
+					<div class="col-sm-4">Phone No: </div>
+					<div class="col-sm-8"><input style="width:300px; border: 2px solid black;"  type="text" id="fphone" name="Phone"></div>
+				</div>
+				<div>
+					<button style=" border :none; background-color:#cc0000; width: 100px; height: 30px; margin-left: 350px; margin-top: 30px; ">
+						<p style=" padding-top:2px; font-size:15px; font-family:cursive; text-align:center; color: white;">Finish</p>
+					</button>
+				</div>
+			</div>
+			</div>
+		</form>
+		
+
+	</div>
+</body>
+</html>
