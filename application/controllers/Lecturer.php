c0a38055539f24b3087ecc571d112b2fda63e039
syitilv <syitiliviani3@gmail.com>
2019-09-09 10:16:38 +0700

    lecturer detail page progress

--- application/controllers/Lecturer.php
+++ application/controllers/Lecturer.php
@@ -5,8 +5,8 @@ class Lecturer extends CI_Controller {
 
 	public function index()
 	{
-			$data['title'] = "Lecturer";
-			$this->load->view('template/header_lecturer',$data);
+		$data['title'] = "Lecturer";
+		$this->load->view('template/header_lecturer',$data);
     	$this->load->view('announcement_detail_page_lecturer');
     	$this->load->view('template/footer');
 	}
--- application/views/announcement_detail_page_lecturer.php
+++ application/views/announcement_detail_page_lecturer.php
@@ -2,7 +2,6 @@
   <style type="text/css">
     #attribute{
       font-family: cursive;
-      height: 120px;
       margin-top: 20px;
       margin-left: auto;
       margin-right: auto;
@@ -16,6 +15,7 @@
       padding-bottom: auto;
       margin-right: 20px;
       text-align: center;
+      height: 140px;
     }
 
     #title{
@@ -24,17 +24,22 @@
       padding-top: 40px;
       padding-bottom: auto;
       margin-right: 20px;
+      height: 140px;
     }
 
-    #book{
-      width: 8%;
-      height: 80%;
+    #icon-top{
+      width: 15%;
+      height: 75%;
     }
 
-    #card{
-      width: 8%;
-      height: 60%;
-      margin-left: 30%;
+    #icon-bot{
+      width: 15%;
+      height: 50%;
+    }
+
+/*    #card{
+      width: 20%;
+      height: 75%;
     }
 
     #hat{
@@ -46,22 +51,48 @@
       width: 20%;
       height: 80%;
       margin-left: -25%;
-    }
+    }*/
 
-    #base_icon{
+    #box-book{
+      padding-top: 20px;
       background-color: white;
       color: black;
-      padding-top: 0;
-      padding-bottom: 40px;
+      height: 70px;
     }
 
-    #icon-bot{
+    #box-card{
+      padding-left: 1%;
+      margin-left: -4%;
+      padding-top: 20px;
+      background-color: white;
+      color: black;
+      height: 70px;
+    }
+
+    #box-hat{
+      padding-top: 30px;
+      background-color: white;
+      color: black;
+      height: 80px;
+      margin-top: -9%;
+    }
+
+    #box-clock{
+      padding-left: 1%;
+      margin-left: -4%;
+      padding-top: 30px;
+      background-color: white;
+      color: black;
+      height: 80px;
+      margin-top: -9%;
+    }
+/*    #icon-bot{
       background-color: white;
       margin-left: 115px;
       margin-top: -45px;
       font-family: cursive;
     }
-
+*/
   </style>
 </head>
 <body>
@@ -75,25 +106,37 @@
         <a style="text-decoration: underline;font-weight: bold;"> Rekrutmen Asisten Dosen Kalkulus IIB (FIF) </a></br>
         <a> posted by M. Ikazra ST, MT.</a>
       </div>
-      <div class="col-md-5" id="base_icon">
-        <img src="<?php echo base_url('assets/image/book.png') ?>" height="60" alt="" id="book"> Kalkulus IIB
-        <img src="<?php echo base_url('assets/image/card.png') ?>" height="60" alt="" id="card"> Asisten Dosen
+        <div class="col-md-3" id="box-book">
+          <img src="<?php echo base_url('assets/image/book.png') ?>" alt="" id="icon-top"> Kalkulus IIB
+        </div>
+        <div class="col-md-3" id="box-card">
+        <img src="<?php echo base_url('assets/image/card.png') ?>"alt="" id="icon-top"> Asisten Dosen
       </div>
     </div>
-    <div class="row">
-      <div class="col-md-1">
+    <div class="row" id="attribute">
+      <div class="col-md-1" style="padding-top: 40px;
+      padding-bottom: auto;
+      margin-right: 20px;
+      text-align: center;
+      height: 140px;">
       </div>
-      <div class="col-md-4" style="margin-right: 20px">
+      <div class="col-md-5" style="padding-top: 40px;
+      padding-bottom: auto;
+      margin-right: 20px;
+      height: 140px;">
       </div>
-      <div class="col-md-2" id="icon-bot">
-        <img src="<?php echo base_url('assets/image/hat.png') ?>" height="60" alt="" id="hat"> FIF
-      </div>
-      <div class="col-md-2" style="margin-left: 110px;margin-top: -4%;font-family: cursive;">
-        <img src="<?php echo base_url('assets/image/clock.png') ?>" height="60" alt="" id="clock"> Semester Genap
+        <div class="col-md-3" id="box-hat">
+          <img src="<?php echo base_url('assets/image/hat.png') ?>" alt="" id="icon-bot"> FIF
+        </div>
+        <div class="col-md-3" id="box-clock">
+        <img src="<?php echo base_url('assets/image/clock.png') ?>"alt="" id="icon-bot"> Semester Genap 2018/2019
       </div>
     </div> 
-    <div class="row" style="height: 500px;width: 100%;background-color: white;margin-left: auto;margin-right: auto; margin-top: 20px;">
-
+    <div class="row" id="attribute" style="height: auto;width: 100%;background-color: white;margin-left: auto;margin-right: auto; margin-top: -12%;">
+        lorem ipsum dolor amet
+    </div>
+    <div class="row" id="attribute" style="height: auto;width: 100%;background-color: #dbdad7;margin-left: auto;margin-right: auto; margin-top:0;padding-left: 50%;padding-right: 50%">
+      hhehhee
     </div>
   </div>
 </body>
